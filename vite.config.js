810813d2cc5a85e43f24c94bf2dbb1ed16c44f37
KimV <71965590+dezineskv@users.noreply.github.com>
2024-11-03 21:19:45 -0800

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/figma-sass-ecommerce/",
-  plugins: [react()],
-  server: {
-    host: true,
-  },
+  plugins: [react()]
 })
